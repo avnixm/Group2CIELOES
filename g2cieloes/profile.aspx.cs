7de9ed14da22e988f8364cd9fc62f49e6258a2bd
BEnJIEBALNEG <164105755+BEnJIEBALNEG@users.noreply.github.com>
2024-05-31 13:58:47 +0800

    Update profile.aspx.cs
    update ko lang para pwede na magchange ng name gamit yung textbow pero yung email lang hindi gumagana

--- g2cieloes/profile.aspx.cs
+++ g2cieloes/profile.aspx.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Linq;
 
 namespace g2cieloes
 {
@@ -13,7 +15,6 @@ namespace g2cieloes
         {
             if (!IsPostBack)
             {
-
                 DisplayUserXPAndHearts();
                 DisplayNameAndJoinDate();
 
@@ -52,7 +53,123 @@ namespace g2cieloes
                 User user = (User)Session["User"];
                 string fullName = $"{user.FirstName} {user.LastName}";
                 firstnamelabel.Text = fullName;
-                datejoinedlabel.Text = $"Date Joined: {user.RegistrationDate.ToString("MMM dd, yyyy")}";
+                datejoinedlabel.Text = $"Date Joined: {user.RegistrationDate:MMM dd, yyyy}";
+                p_username.Text = user.Email;
+                p_name.Text = fullName;
+            }
+        }
+
+        protected void SaveChanges(object sender, EventArgs e)
+        {
+            if (Session["User"] != null)
+            {
+                User user = (User)Session["User"];
+                string newEmail = p_username.Text;
+                string newName = p_name.Text;
+
+                if (newEmail != user.Email)
+                {
+                    // Send OTP to the current email
+                    SendOTP(user.Email);
+                    // Save new email to the session for verification later
+                    Session["NewEmail"] = newEmail;
+                }
+                else
+                {
+                    // Update name in the database
+                    UpdateUserName(user.UserId, newName);
+                    user.FirstName = newName.Split(' ')[0];
+                    user.LastName = newName.Split(' ').Length > 1 ? newName.Split(' ')[1] : "";
+                }
+
+                // Update the session user
+                Session["User"] = user;
+            }
+        }
+
+        protected void p_name_TextChanged(object sender, EventArgs e)
+        {
+            if (Session["User"] != null)
+            {
+                User user = (User)Session["User"];
+                string newName = p_name.Text;
+
+                if (newName != $"{user.FirstName} {user.LastName}")
+                {
+                    UpdateUserName(user.UserId, newName);
+                    user.FirstName = newName.Split(' ')[0];
+                    user.LastName = newName.Split(' ').Length > 1 ? newName.Split(' ')[1] : "";
+                    Session["User"] = user;
+                }
+            }
+        }
+
+        protected void p_username_TextChanged(object sender, EventArgs e)
+        {
+            if (Session["User"] != null)
+            {
+                User user = (User)Session["User"];
+                string newEmail = p_username.Text;
+
+                if (newEmail != user.Email)
+                {
+                    // Send OTP to the current email
+                    SendOTP(user.Email);
+                    // Save new email to the session for verification later
+                    Session["NewEmail"] = newEmail;
+                }
+            }
+        }
+
+        private void SendOTP(string email)
+        {
+            // Implement OTP sending logic here
+        }
+
+        private void UpdateUserName(int userId, string newName)
+        {
+            // Implement the database update logic here
+            // Example:
+            string connectionString = "Server=localhost;user=root;database=g2cieloes;password=";
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                 var command = new MySqlCommand("UPDATE userinfo SET user_fname = @FirstName, user_lname = @LastName WHERE UserId = @UserId", conn);
+                 command.Parameters.AddWithValue("@FirstName", newName.Split(' ')[0]);
+                 command.Parameters.AddWithValue("@LastName", newName.Split(' ').Length > 1 ? newName.Split(' ')[1] : "");
+                 command.Parameters.AddWithValue("@UserId", userId);
+                 conn.Open();
+                 command.ExecuteNonQuery();
+             }
+        }
+
+        private void UpdateUserEmail(int userId, string newEmail)
+        {
+            // Implement the database update logic here
+            // Example:
+            // using (var connection = new SqlConnection("your_connection_string"))
+            // {
+            //     var command = new SqlCommand("UPDATE Users SET Email = @Email WHERE UserId = @UserId", connection);
+            //     command.Parameters.AddWithValue("@Email", newEmail);
+            //     command.Parameters.AddWithValue("@UserId", userId);
+            //     connection.Open();
+            //     command.ExecuteNonQuery();
+            // }
+        }
+
+        protected void VerifyOTP(string otp)
+        {
+            string sessionOtp = Session["OTP"] as string;
+            if (otp == sessionOtp)
+            {
+                User user = (User)Session["User"];
+                string newEmail = Session["NewEmail"] as string;
+                UpdateUserEmail(user.UserId, newEmail);
+                Session["NewEmail"] = null;
+                Session["OTP"] = null;
+            }
+            else
+            {
+                // Handle invalid OTP case
             }
         }
     }
